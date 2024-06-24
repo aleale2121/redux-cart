c1cbe27407a3b8891ab4e7730c7cb523648bed93
alefew <alefewyimer4@gmail.com>
2024-06-24 16:46:13 +0300

    fetching data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import { useEffect } from "react";
+import { Fragment, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
 import Notification from "./components/UI/Notification";
-import { sendCartData } from "./store/cart-slice";
+import { sendCartData, fetchCartData } from "./store/cart-actions";
+
 let isInitial = true;
 
 function App() {
@@ -15,15 +16,22 @@ function App() {
   const notification = useSelector((state) => state.ui.notification);
 
   useEffect(() => {
+    dispatch(fetchCartData());
+  }, [dispatch]);
+
+  useEffect(() => {
     if (isInitial) {
       isInitial = false;
       return;
     }
-    dispatch(sendCartData(cart));
+
+    if (cart.changed) {
+      dispatch(sendCartData(cart));
+    }
   }, [cart, dispatch]);
 
   return (
-    <>
+    <Fragment>
       {notification && (
         <Notification
           status={notification.status}
@@ -35,7 +43,7 @@ function App() {
         {showCart && <Cart />}
         <Products />
       </Layout>
-    </>
+    </Fragment>
   );
 }
 
